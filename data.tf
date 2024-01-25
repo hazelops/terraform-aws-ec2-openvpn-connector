df51aef151c21a870ad4bb5e927e5bd30677845f
Vlad Borovtsov <borovcov.vlad@gmail.com>
2024-01-25 14:54:36 +0100

    drop template and local from versions; using built-in function templatefile instead of deprecated template provider

--- data.tf
+++ data.tf
@@ -2,18 +2,10 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-data "template_file" "ec2_user_data" {
-  template = file("${path.module}/ec2_user_data.sh.tpl")
-
-  vars = {
-    openvpn_token = var.openvpn_token
-  }
-}
-
 data "aws_ami" "ubuntu_22_04" {
   most_recent = true
   filter {
-    name   = "name"
+    name = "name"
     # Ubuntu 22.04 LTS amd64 image in us-east-1. If upgrade is needed, you would need to re-connect to openvpn.
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230601"]
   }
